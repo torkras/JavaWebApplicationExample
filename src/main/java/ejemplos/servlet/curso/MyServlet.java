874263c4c127938eca1fa9640d08a136d060d94f
torkras <149871061+torkras@users.noreply.github.com>
2023-12-05 02:17:20 +0100

    Añadidos los cambios para la entrega de Actividad 1. Servlets.

--- src/main/java/ejemplos/servlet/curso/MyServlet.java
+++ src/main/java/ejemplos/servlet/curso/MyServlet.java
@@ -19,12 +19,24 @@ public class MyServlet extends HttpServlet {
 
 		// send HTML page to client
 		out.println("<html>");
-		out.println("<head><title>Ejemplo HTML desde Servlet</title></head>");
+		out.println("<head><title>Actividad 1. Servlets - Send</title></head>");
 		out.println("<body>");
-		out.println("<h1>Ejemplo Servlet</h1>");
-		out.println("<p>Este es un ejemplo en el curso de Java para generar HTML desde un Servlet.</p>");
-		out.println("<p><a href=\"/myServlet2\">Vamos al otro Servlet</a></p>");
-		out.println("</body></html>");
+		out.println("<h1>Paso de parámetros entre Servlets</h1>");
+		out.println("<p>Hay que crear un formulario sencillo en HTML, basta con que tenga un campo de texto y el botón de enviar.</p>");
+		out.println("<form action=\"/myServlet2\">");
+		out.println("<label for=\"fname\">Nombre:</label><br>");
+		out.println("<input type=\"text\" id=\"fname\" name=\"fname\" value=\"Juan\"><br>");
+		out.println("<label for=\"lname\">Apellidos:</label><br>");
+		out.println("<input type=\"text\" id=\"lname\" name=\"lname\" value=\"Exposito\"><br><br>");
+		out.println("<input type=\"radio\" id=\"male\" name=\"sex\" value=\"male\" checked>");
+		out.println("<label for=\"male\">Hombre</label>");
+		out.println("<input type=\"radio\" id=\"female\" name=\"sex\" value=\"female\">");
+		out.println("<label for=\"female\">Mujer</label><br><br>");
+		out.println("<input type=\"submit\" value=\"Enviar\">");
+		out.println("<input type=\"reset\" value=\"Limpiar\">");
+		out.println("</form>");
+		out.println("</body>");
+		out.println("</html>");
 	}
 
 }
--- src/main/java/ejemplos/servlet/curso/MyServlet2.java
+++ src/main/java/ejemplos/servlet/curso/MyServlet2.java
@@ -14,14 +14,30 @@ public class MyServlet2 extends HttpServlet {
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 
+		String welcomeWord;
+
+		if (request.getParameter("sex").equals("male")) {
+			welcomeWord = "Bienvenido";
+		} else if (request.getParameter("sex").equals("female")) {
+			welcomeWord = "Bienvenida";
+		} else {
+			welcomeWord = "Bienvenid@";
+		}
+
+
+
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
 
 		// send HTML page to client
 		out.println("<html>");
-		out.println("<head><title>Ejemplo HTML desde Servlet</title></head>");
+		out.println("<head><title>Actividad 1. Servlets - Receive</title></head>");
 		out.println("<body>");
-		out.println("<h1>Bienvenido!!</h1>");
+		out.println("<h1>" + welcomeWord +
+				" " + request.getParameter("fname") + " " + request.getParameter("lname") +
+				"!!" + "</h1>");
+		out.println("</body>");
+		out.println("</html>");
 	}
 
 }
